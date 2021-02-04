5793f8f7c1e6a3417b0dd3bf50baba439febe62b
Miofly <953212389@qq.com>
2021-02-04 22:30:10 +0800

    1

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-const getSpecialDays1 = (y: number) => {
+const getSpecialDays1 = (y: number): number => {
 	if (y % 400 == 0 || y % 4 == 0 && y % 100 != 0) {
-		return 29;
+		return 29
 	}
-	return 28;
-};
+	return 28
+}
 
-const getSpecialDaysTwo = (y: number) => { // 判断是否是余年
+const getSpecialDaysTwo = (y: number): number => { // 判断是否是余年
 	if (y % 400 == 0 || y % 4 == 0 && y % 100 != 0) {
-		return 29;
+		return 29
 	}
-	return 28;
-};
+	return 28
+}
 
 export {
 	getSpecialDaysTwo,
 	getSpecialDays1
-};
+}
