5a17ef1769cc1213c63f6d1f77ae418c39efcec4
Miofly <953212389@qq.com>
2021-02-05 21:35:09 +0800

    wfd_update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 const roots = {
 	// 信息提示
 	showToast (title = '操作成功', icon: string | boolean = 'none', duration = 1300, mask = false): void {
-		// @ts-ignore
 		uni.showToast({ title, icon, duration, mask })
 	},
 	// loading设置
-	showLoading (title = '正在加载', mask = false): void {
+	showLoading (title = '正在加载', mask = false) {
 		uni.showLoading({
 			title,
 			mask // 防止触摸穿透
